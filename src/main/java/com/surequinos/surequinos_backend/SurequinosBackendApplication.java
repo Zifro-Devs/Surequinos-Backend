aa87e7bf8efe1ca897b518b01c7b9d9b9fefb709
KevDev <kevin.estrada1@udea.edu.co>
2025-10-31 11:47:02 -0500

    Versión Inicial

---
+++ src/main/java/com/surequinos/surequinos_backend/SurequinosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.surequinos.surequinos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurequinosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurequinosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surequinos/surequinos_backend/SurequinosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surequinos.surequinos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurequinosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

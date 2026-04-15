e75613e982a1542394e90b0863d4da98e85354af
Juan Andrés <juan.rivera7@udea.edu.co>
2026-04-14 21:28:09 -0500

    fix: despliegue en railway

--- src/test/java/com/surequinos/surequinos_backend/SurequinosBackendApplicationTests.java
+++ src/test/java/com/surequinos/surequinos_backend/SurequinosBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.surequinos.surequinos_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SurequinosBackendApplicationTests {
 
 	@Test
